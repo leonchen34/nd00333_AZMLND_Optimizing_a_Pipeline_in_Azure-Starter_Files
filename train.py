b0ead490b3f00ca65dbb346f46c8525511092e5d
leonchen34 <leonchen34@gmail.com>
2023-06-26 23:10:14 -0500

    Add files via upload
    Updates after more test.

--- train.py
+++ train.py
@@ -64,7 +64,6 @@ def main():
 
     # TODO: Split data into train and test sets.
 
-    ### YOUR CODE HERE ###a
     x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.3,random_state=223)
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
